3ceb159ecbdc44f6ebd3b4f0b3b7c53d730b0af4
ken <kenokabe@gmail.com>
2014-02-05 14:54:39 +0900

    commit

--- test.js
+++ test.js
@@ -8,7 +8,6 @@ var expect = require('chai')
 
 var M = require('./app.js');
 
-
 describe('=============================== SpaceTime TEST =====================================',
     function()
     {
