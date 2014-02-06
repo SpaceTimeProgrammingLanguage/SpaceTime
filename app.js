323f6b50d63181decc1c125995771fe1bdd74765
ken <kenokabe@gmail.com>
2014-02-07 04:59:03 +0900

    commit

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-/* jslint node: true */
-/* global describe, it, before, beforeEach, after, afterEach */
+     var SpaceTime_FunctionsDIR = './SpaceTime_Functions/';
+     var SpaceTime_coreFile = '_core.js';
+     console.log('{src f}   src -f-> ??');
+     console.log('');
+     console.log('SpaceTime modlue loading...');
+      //var M = require(SpaceTime_FunctionsDIR + SpaceTime_coreFile);
+      //require(STRING) must be raw STRING to be read by Browserify
+     var M = require('./SpaceTime_Functions/_core.js');
+     module.exports = M;
+     console.log('core module');
 
-'use strict';
+     var loadModulesFactory;
 
-var SpaceTime_FunctionsDIR = './SpaceTime_Functions/';
-var SpaceTime_coreFile = '_core.js';
-console.log('{src f}   src -f-> ??');
-console.log('');
-console.log('SpaceTime modlue loading...');
-//var M = require(SpaceTime_FunctionsDIR + SpaceTime_coreFile);
-//require(STRING) must be raw STRING to be read by Browserify
-var M = require('./SpaceTime_Functions/_core.js');
-module.exports = M;
-console.log('core module');
+     if (typeof window === 'undefined')
+     {
+       var modulePathHiddenFromBrowserify = './loadModulesFactory';
+       loadModulesFactory = require(modulePathHiddenFromBrowserify);
+     }
+     else
+     {
+       loadModulesFactory = require('./loadModulesFactoryBrowserify');
+     }
 
-var loadModulesFactory;
+     var obj = loadModulesFactory(SpaceTime_FunctionsDIR, SpaceTime_coreFile, M);
+     M = obj.M;
+     var loadModules = M.loadModules = obj.func;
 
-if (typeof window === 'undefined')
-{
-  var modulePathHiddenFromBrowserify = './loadModulesFactory';
-  loadModulesFactory = require(modulePathHiddenFromBrowserify);
-}
-else
-{
-  loadModulesFactory = require('./loadModulesFactoryBrowserify');
-}
+     if (typeof describe === 'undefined')
+     {
+       loadModules(function()
+       {
+         M.$W('------------- SpaceTime ready ----------------');
 
-var obj = loadModulesFactory(SpaceTime_FunctionsDIR, SpaceTime_coreFile, M);
-M = obj.M;
-var loadModules = M.loadModules = obj.func;
-
-if (typeof describe === 'undefined')
-{
-  loadModules(function()
-  {
-    M.$W('------------- SpaceTime ready ----------------');
-
-    var myF1 =
+         var myF1 =
             [
                   M.FUNCTION_COMPOSITION,
                   [M.plus, M.VAL(0)],
@@ -44,7 +39,7 @@ if (typeof describe === 'undefined')
                   [M.plus, M.VAL(2)]
             ];
 
-    var code =
+         var code =
             [
                   1,
                  [myF1, [[2], [3], [4]]],
@@ -52,11 +47,11 @@ if (typeof describe === 'undefined')
                  [M.map, [M.CONSOLE]]
             ];
 
-    M.$mapMEMORY(code);
+         M.$mapMEMORY(code);
 
 
-    //------------------------------------------------------------------
-  });
-}
+         //------------------------------------------------------------------
+       });
+     }
 
-//=========================================
+      //=========================================
