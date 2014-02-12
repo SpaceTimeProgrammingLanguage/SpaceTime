729931e77b131b8c1ea246245b1a69d629768111
ken <kenokabe@gmail.com>
2014-02-13 08:52:00 +0900

    commit

--- SpaceTime_Functions/ifF.js
+++ SpaceTime_Functions/ifF.js
@@ -13,22 +13,15 @@ var ifF = function(src, atr)
   M.$L(M.$type(src) === 'Array');
 
   M.$L('!!atr!!');
-  M.$L(atr[0]); // [true]
+  M.$L(atr[0]); // [true] 
 
-  var boolF = function(bool)
-  {
-
-    return M.$content(M.$mapEVAL(bool));
-
-  };
-
-  if (boolF(atr[0]))
+  if (M.$content(M.$mapEVAL(atr[0])))
   {
     return M.$mapEVAL(atr[1]);
   }
   else
   {
-    return [M.$content(M.$mapEVAL(src))];
+    return M.$mapEVAL(src);
   }
 
 };
